815a3f86f2a656ffa30500ee31ad2f34cd629513
root <mutabazijimmy9@gmail.com>
2022-05-31 17:36:55 -0700

    convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) in range(97, 123):
+            diff = 32
+        else:
+            diff = 0
+        print("{:c}".format(ord(str[i]) -diff), end="")
+    print()
