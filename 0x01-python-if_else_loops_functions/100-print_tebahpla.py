350000c183830f052c6dd5bf47cb24ec84e836e1
root <mutabazijimmy9@gmail.com>
2022-05-31 22:42:28 -0700

    rev and mix alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+i = 122
+while i >= 97:
+    if i % 2 != 0:
+        var = i - 32
+    else:
+        var = i
+    print("{:c}".format(var), end="")
+    i -= 1
