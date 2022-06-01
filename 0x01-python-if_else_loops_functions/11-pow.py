259d664fcdf3e0505227b5f29183d3aa4f31c41d
root <mutabazijimmy9@gmail.com>
2022-05-31 19:44:39 -0700

    power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    power = 1
-    if b < 0:
-        b = -b
-        for i in range(b):
-            power = float(power) / a
-    elif b == 0:
-        power = 1
-    else:
-        for i in range(b):
-            power *= a
+    power = a ** b
     return power
