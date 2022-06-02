dda6be2e2028244cd3414595a0e6aaafc42b28cf
root <mutabazijimmy9@gmail.com>
2022-06-02 16:53:08 -0700

    modules

---
+++ 0x01-python-if_else_loops_functions/11-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+pow = __import__('11-pow').pow
+
+print(pow(3, 3))
+print(pow(-98, -10))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
+print(pow(-98, -10))
