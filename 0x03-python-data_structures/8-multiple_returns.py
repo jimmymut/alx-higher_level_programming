ee0c10466502e97f9cb6a4ab8122fde376143ccd
root <mutabazijimmy9@gmail.com>
2022-06-06 18:51:27 -0700

    mult returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     len_str = len(sentence)
 
     if (len_str == 0):
-        new_tuple = (len_str None)
+        new_tuple = (len_str, None)
     else:
         new_tuple = (len_str, sentence[0])
 
