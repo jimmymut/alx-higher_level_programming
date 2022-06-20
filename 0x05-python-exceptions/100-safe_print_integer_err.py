02def1ee36ebcb1af62eaec0fb5fc14507b9904c
root <mutabazijimmy9@gmail.com>
2022-06-20 15:21:54 -0700

    print integer and write error message to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as error:
+        sys.stderr.write("Exception: {}".format(error))
+        return False
+    else:
+        return True
