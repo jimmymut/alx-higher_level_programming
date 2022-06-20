2491d0f34a4aeb357c3ca3d0f5e9057adff5496d
root <mutabazijimmy9@gmail.com>
2022-06-20 06:04:15 -0700

    my first commit to exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,7 @@ def safe_print_list(my_list=[], x=0):
         return x
     except:
         print()
-        pass
-    return len(my_list)
+        x = 0
+        for i in my_list:
+            x += 1
+    return x
