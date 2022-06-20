11e2fb9b586e3f4a9f4729078b731286ded3e668
root <mutabazijimmy9@gmail.com>
2022-06-20 10:03:47 -0700

    printing integers only in the list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            if not type(my_list[i]) == int:
-                continue
+    count = 0
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        print()
-        return count
-    except (ValueError,TypeError):
-        pass
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
